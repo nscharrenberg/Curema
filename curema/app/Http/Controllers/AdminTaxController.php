c80a3081d07fb06e40593e27e02ddedc02e05cc8
nscharrenberg <n.scharrenberg@student.fontys.nl>
2017-11-18 12:57:09 +0100

    :ok_hand: Improved Tax Controller
    Added the AdminTaxController for CRUD functionality.

---
+++ curema/app/Http/Controllers/AdminTaxController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tax;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class AdminTaxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $taxes = Tax::all();
+        return view('admin.taxes.index', compact('taxes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.taxes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $tax = Tax::create($input);
+        Session::flash('created_tax', 'The tax ' . $tax->name . ' - ' . $tax->rate . '% has been created');
+        return redirect('/admin/taxes');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $tax = Tax::findOrFail($id);
+        return view('admin.taxes.edit', compact('tax'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $input = $request->all();
+        $tax = Tax::findOrFail($id);
+        $tax->update($input);
+        Session::flash('updated_tax', 'The Tax ' . $tax->name . ' - ' . $tax->rate . '% has been updated');
+        return redirect('/admin/taxes');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $type = Tax::findOrFail($id);
+        $type->delete();
+        Session::flash('deleted_tax', 'Tax has been deleted!');
+        return redirect('/admin/taxes');
+    }
+}
