9f0165c1a6d5b75b5b5b3e4bc598797194cf5bb5
nscharrenberg <n.scharrenberg@student.fontys.nl>
2017-11-26 16:01:52 +0100

    :ok_hand: Clean Welcome Page.

--- curema/resources/views/welcome.blade.php
+++ curema/resources/views/welcome.blade.php
@@ -66,48 +66,30 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-
-
-
-                <div class="top-right links">
-                    @if(Auth::guard('admin')->check())
-                        <a href="{{ route('admin.customer.index') }}">Admin Panel</a>
-                    @endif
-                    @if (Route::has('login'))
-                        @auth
-                            <a href="{{ url('/home') }}">Home</a>
+            <div class="top-right links">
+                <a href="{{ route('admin.customer.index') }}">Admin Panel</a>
+                @if (Route::has('login'))
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
                         @else
                             <a href="{{ route('login') }}">Client Login</a>
-                        @endauth
-                    @endif
-                </div>
-
-
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
-                        <div class="panel">
-                            @component('components.who')
-
-                                @endcomponent
-                        </div>
-                    </div>
-                </div>
+                            @endauth
+                        @endif
             </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+                {{--<div class="top-right links">--}}
+                    {{--@if(Auth::guard('admin')->check())--}}
+                        {{--<a href="{{ route('admin.customer.index') }}">Admin Panel</a>--}}
+                    {{--@endif--}}
+                    {{--@if (Route::has('login'))--}}
+                        {{--@auth--}}
+                            {{--<a href="{{ url('/home') }}">Home</a>--}}
+                        {{--@else--}}
+                            {{--<a href="{{ route('login') }}">Client Login</a>--}}
+                        {{--@endauth--}}
+                    {{--@endif--}}
+                {{--</div>--}}
         </div>
     </body>
 </html>
